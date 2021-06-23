0333be89302016978bc0d76079942ac4d4359e2a
zavanton123 <zavanton123@gmail.com>
2021-06-23 11:52:54 +0300

    ingress is added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,17 @@ import {json} from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/', (req, res) => {
+  console.log(`zavanton - hello world`);
+  res.send('hello world');
+});
+
+app.get('/api/users/currentuser', (req, res) => {
+  console.log(`zavanton - current user`);
+  res.send('Hi there');
+});
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!!');
+  console.log(`zavanton - version 2`);
+  console.log('Listening on port 3000');
 });
