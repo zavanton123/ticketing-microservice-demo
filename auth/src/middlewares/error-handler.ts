5a295fa30bf2c1e5bbdc50be34efcc8a702dcb8a
zavanton123 <zavanton123@gmail.com>
2021-06-23 18:08:53 +0300

    error handling updated

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import {NextFunction, Request, Response} from "express";
-import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
+import {CustomError} from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,17 +7,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    console.log(`zavanton - handling request validation error`);
-    const formattedErrors = err.errors.map(error => {
-      return {message: error.msg, field: error.param};
-    });
-    return res.status(400).send({errors: formattedErrors});
-  }
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({
-      errors: [{message: err.reason}]
-    });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({errors: err.serializeErrors()});
   }
 
   res.status(400).send({
