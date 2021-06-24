ea48ae1a90e5de2dab0e61cc4b6eae6a30549025
zavanton123 <zavanton123@gmail.com>
2021-06-24 17:14:19 +0300

    login is added

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,40 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
+import {validateRequest} from "../middlewares/validate-request";
+import {User} from "../models/user";
+import {BadRequestError} from "../errors/bad-request-error";
+import {Password} from "../services/password";
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there');
-});
+router.post('/api/users/signin', [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const {email, password} = req.body;
+    const existingUser = await User.findOne({email});
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+    const userJwt = jwt.sign({id: existingUser.id, email: existingUser.email},
+      // the ! is used to avoid typescript checks
+      process.env.JWT_KEY!
+    )
+    req.session = {jwt: userJwt};
+    res.status(200).send(existingUser);
+  });
 
 export {router as signinRouter};
