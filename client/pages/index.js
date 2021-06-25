9d03d91d67f12036eb10c9afea6d66254a3af9f6
zavanton123 <zavanton123@gmail.com>
2021-06-25 18:08:18 +0300

    domain and cookie problem for the client fixed

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,32 @@
-export default () => {
-  return <h1>Landing Page</h1>
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+  console.log(`zavanton - current user`);
+  console.log(currentUser);
+
+  return <>
+    <h1>Landing Page</h1>
+  </>
 }
+
+LandingPage.getInitialProps = async ({req}) => {
+  console.log(`zavanton - headers`);
+  console.log(req.headers);
+
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to
+    // http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+    const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+      headers: req.headers
+    });
+    return data;
+  } else {
+    // we are on the browser
+    // request can be made with a base url of ''
+    const {data} = await axios.get('/api/users/currentuser')
+    return data;
+  }
+};
+
+export default LandingPage;
