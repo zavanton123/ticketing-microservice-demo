0f9f247db25ada1bfe957b1fd8ad687fb9f4e79c
zavanton123 <zavanton123@gmail.com>
2021-06-25 16:37:11 +0300

    onSuccess added

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
 export default () => {
   const [email, setEmail] = useState('');
@@ -7,13 +8,14 @@ export default () => {
   const {doRequest, errors} = useRequest({
     url: '/api/users/signup',
     method: 'post',
-    body: {email, password}
+    body: {email, password},
+    onSuccess: () => Router.push('/')
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
   }
 
   return (
