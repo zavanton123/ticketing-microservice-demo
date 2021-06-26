91e79f8ae97be89c9714a5d3b492a88588168e3c
zavanton123 <zavanton123@gmail.com>
2021-06-26 11:22:40 +0300

    signin is working

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,5 +2,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 // this wrapper is necessary if we want to add some global css to our components
 export default ({Component, pageProps}) => {
-  return <Component {...pageProps}/>
+  return <div>
+    <h1>Header!</h1>
+    <Component {...pageProps}/>
+  </div>
 };
