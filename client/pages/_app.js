da37f9f045a6837fe7954c258361d5595bf02c63
zavanton123 <zavanton123@gmail.com>
2021-06-26 11:48:15 +0300

    AppComponent is calling getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
 // this wrapper is necessary if we want to add some global css to our components
-export default ({Component, pageProps}) => {
+const AppComponent = ({Component, pageProps, currentUser}) => {
   return <div>
-    <h1>Header!</h1>
+    <h1>Header! {currentUser.email}</h1>
     <Component {...pageProps}/>
   </div>
 };
+
+AppComponent.getInitialProps = async (appContext) =>  {
+  const client = buildClient(appContext.ctx);
+  const {data} = await client.get('/api/users/currentuser');
+
+  // Manually call LandingPage's getInitialProps method
+  // (because it is not called by default)
+  let pageProps = {};
+  // here we are checking if the component has getInitialProps defined.
+  // (e.g. LandingPage has getInitialProps, but other components have none)
+  if(appContext.Component.getInitialProps){
+    // call the component's getInitialProps
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  // return the AppComponent getInitialProps data
+  // and component's getInitialProps data
+  return {
+    pageProps,
+    // the same as: currentUser: data.currentUser
+    ...data
+  }
+};
+
+export default AppComponent;
