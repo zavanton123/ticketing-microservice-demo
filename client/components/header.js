2f11ab8002d9a1d686a4609a1c750bc62b58f306
zavanton123 <zavanton123@gmail.com>
2021-06-26 12:13:43 +0300

    signout ok

--- client/components/header.js
+++ client/components/header.js
@@ -1,14 +1,28 @@
 import Link from 'next/link';
 
 export default ({currentUser}) => {
+  const links = [
+    !currentUser && {label: 'Sign up', href: '/auth/signup'},
+    !currentUser && {label: 'Sign in', href: '/auth/signin'},
+    currentUser && {label: 'Sign out', href: '/auth/signout'},
+  ]
+    .filter(linkConfig => linkConfig)
+    .map(({label, href}) => {
+      return <li key={href} className="nav-item">
+        <Link href={href}>
+          <a className="nav-link">{label}</a>
+        </Link>
+      </li>
+    });
+
   return <nav className="navbar navbar-light bg-ligh">
-    <Link href="/" >
+    <Link href="/">
       <a className="navbar-brand">GitTix</a>
     </Link>
 
     <div className="d-flex justify-content-end">
       <ul className="nav d-flex align-items-center">
-        {currentUser ? 'Sign Out' : 'Sign in/up'}
+        {links}
       </ul>
     </div>
   </nav>
