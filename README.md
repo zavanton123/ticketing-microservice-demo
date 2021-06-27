aedcd03e260314361bf309462daf26c8c0469b0b
zavanton123 <zavanton123@gmail.com>
2021-06-27 14:22:00 +0300

    some listener logic added

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ### Port Forwarding for dev purposes (in K8S cluster)
 ### (good for simulating connection break up)
 kubectl port-forward nats-depl-c576ff5c8-4mq6p 4222:4222
+kubectl port-forward nats-depl-c576ff5c8-4mq6p 8222:8222
+
+### You can monitor client state at
+### http://localhost:8222/streaming
+
+### You can check info about clients connected to some channel
+### http://localhost:8222/streaming/channelsz?subs=1
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,50 @@
-import nats from 'node-nats-streaming';
+import nats, {Message} from 'node-nats-streaming';
+import {randomBytes} from 'crypto';
 
 // this is to remove the previous output in the console
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+// each client must have a unique id, so
+// we are generating a random id
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
   console.log(`zavanton - listener is connected to NATS`);
 
-  const subscription = stan.subscribe('ticket:created');
-  subscription.on('message', (msg) => {
-    console.log(`zavanton - message received`);
+  // this helps to prevent NATS from waiting for a client
+  // from a killed process
+  stan.on('close', () => {
+    console.log(`zavanton - NATS connection closed`);
+    process.exit();
+  });
+
+  // acks should be sent back manually
+  const options = stan.subscriptionOptions()
+    .setManualAckMode(true);
+
+  // subscribe to some channel
+  // and be a part of some queue group
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
+
+  // listen for messages and process them
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === 'string') {
+      console.log(`Received event number: ${msg.getSequence()} with data: ${data}`);
+    }
+
+    // send back the ack manually
+    // when the message is processed successfully
+    msg.ack();
   });
 });
+
+// close the NATS client before killing the process
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
