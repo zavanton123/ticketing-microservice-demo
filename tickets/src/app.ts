a3e4eb312ada060a92dfea63bee697724e17e9a0
zavanton123 <zavanton123@gmail.com>
2021-06-27 11:18:24 +0300

    index tickets added

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import {errorHandler, NotFoundError, currentUser} from '@zatickets/common';
 import cookieSession from 'cookie-session';
 import {createTicketRouter} from "./routes/new";
 import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
