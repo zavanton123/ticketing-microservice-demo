f86b86d77f4355e02671c747b0acc2aaf34d595f
zavanton123 <zavanton123@gmail.com>
2021-06-29 19:38:26 +0300

    order model created

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,22 @@
+import mongoose from 'mongoose';
 import express, {Request, Response} from 'express';
+import {requireAuth, validateRequest} from '@zatickets/common';
+import {body} from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post('/api/orders', requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // this check is ok, but it created tight coupling with the tickets microservice
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  });
 
 export {router as newOrderRouter};
