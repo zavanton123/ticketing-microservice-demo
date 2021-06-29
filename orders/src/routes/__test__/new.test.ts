0fda32a57c6afa7dbca702bce913b46ceeeec3a6
zavanton123 <zavanton123@gmail.com>
2021-06-29 21:48:52 +0300

    create order test added - more

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
 import {app} from '../../app';
+import {Order, OrderStatus} from '../../models/order';
+import {Ticket} from "../../models/ticket";
 
 
 it('returns an error if the ticket does not exist', async () => {
@@ -13,7 +15,28 @@ it('returns an error if the ticket does not exist', async () => {
 });
 
 it('returns an error if the ticket if already reserved', async () => {
+  // save a ticker
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+  await ticket.save();
 
+  // save an order
+  const order = Order.build({
+    ticket,
+    userId: 'fslajfaskl',
+    status: OrderStatus.Created,
+    expiresAt: new Date()
+  });
+  await order.save();
+
+  // attempt to create order
+  await request(app)
+    .post('/api/orders/')
+    .set('Cookie', global.signin())
+    .send({ticketId: ticket.id})
+    .expect(400);
 });
 
 it('reserves a ticket', async () => {
