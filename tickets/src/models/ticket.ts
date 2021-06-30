c02b050a8069522dfda5f3f22fa18ae8f5e8de89
zavanton123 <zavanton123@gmail.com>
2021-06-30 20:26:34 +0300

    added optimistic concurrency control dependency

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   title: string;
@@ -10,6 +11,8 @@ interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
   userId: string;
+  // for concurrency version control
+  version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -38,10 +41,14 @@ const ticketSchema = new mongoose.Schema({
   }
 });
 
+// enable version increments for event concurrency control
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
-export {Ticket};
+export { Ticket };
