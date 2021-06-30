732903c2575da81396e8de9bfb0f367237be73d7
zavanton123 <zavanton123@gmail.com>
2021-06-30 12:40:25 +0300

    listeners added

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
-import {app} from "./app";
-import {natsWrapper} from "./nats-wrapper";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+import {TicketCreatedListener} from "./events/listeners/ticket-created-listener";
+import {TicketUpdatedListener} from "./events/listeners/ticket-updated-listener";
 
 const start = async () => {
   // check if the environment variables are set
@@ -37,6 +39,10 @@ const start = async () => {
     // disconnect NATS when the application is closed
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    // listen for events
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     // note: use the mongodb pod's cluster ip address: tickets-mongo-srv
     await mongoose.connect(process.env.MONGO_URI, {
