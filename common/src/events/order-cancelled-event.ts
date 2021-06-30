22f9ff59e7a7d73bd22579ad1485b6af9485bed4
zavanton123 <zavanton123@gmail.com>
2021-06-30 20:35:26 +0300

    version added to common module

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -1,11 +1,10 @@
-import {Subjects} from "./subjects";
-import {OrderStatus} from "./types/order-status";
-
+import { Subjects } from "./subjects";
 
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled,
   data: {
     id: string;
+    version: number;
     ticket: {
       id: string;
     };
