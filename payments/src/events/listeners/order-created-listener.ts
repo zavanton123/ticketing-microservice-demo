3d59f9d5262c85da758ada4aa0a1359b9ba74e64
zavanton123 <zavanton123@gmail.com>
2021-07-02 18:40:07 +0300

    order created listener added

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import {Order} from "../../models/order";
+import { Listener, OrderCreatedEvent, Subjects } from "@zatickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
